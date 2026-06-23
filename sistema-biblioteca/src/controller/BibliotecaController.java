a7e753ff4239f1e5479e76a3e0d13e4029440f07
gabsifuentes <gabsifuentesss@gmail.com>
2026-06-23 03:58:07 -0400

    registro de eventos  +  algumas funções

--- sistema-biblioteca/src/controller/BibliotecaController.java
+++ sistema-biblioteca/src/controller/BibliotecaController.java
@@ -32,5 +32,51 @@ public class BibliotecaController {
         carregarDados();
     }
 
+    //botões
+    private void registrarListeners() {
+        telaCadastro.acaoBotaoSalvar(e -> salvarLivro());
+    }
+
+    //cadastra um novo livro vom os dados recebidos da view
+    private void cadastrarLivro() {
+        String titulo = telaCadastroLivro.getTxtTitulo().trim();
+        String autor = telaCadastroLivro.getTxtAutor().trim();
+    }
+
+    if (titulo.isEmpty() || autor.isEmpty()) {
+        JOptionPane.showMessageDialog(
+            telaCadastroLivro,
+            "Preencha todos os campos obrigatórios.",
+            "Erro de validação",
+            JOptionPane.WARNING_MESSAGE
+        );
+        return;
+    }
+
+    //verifica titulo duplicado
+    for(Livro 1: livros) {
+        if (1.getTitulo().equalsIgnoreCasse(titulo)) {
+            JOptionPane.showMessageDialog(
+                telaCadastroLivro,
+            "Já existe um livro com este tírulo!",
+            "Duplicado",
+            JOptionPane.WARNING_MESSAGE
+        );
+        return;
+       }
+    }
+
+    Livro novoLivro = new Livro(titulo, autor, true, proximoIdLivro++);
+    livros.add(novoLivro);
+
+    BibliotecaDAO.salvarLivro(novoLivro);
+
+    JOptionPane.showMessageDialog(
+        telaCadastroLivro,
+        "Livro\"" + titulo + "\"cadastrado com sucesso!",
+        "Sucesso",
+        JOptionPane.INFORMATION_MESSAGE
+    );
+
 
 }
