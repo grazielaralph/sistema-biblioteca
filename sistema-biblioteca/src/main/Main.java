2846304f4f5550c1eb14b2327ad923e24b533a1a
gabsifuentes <gabsifuentesss@gmail.com>
2026-06-23 18:13:39 -0400

    update main

--- sistema-biblioteca/src/main/Main.java
+++ sistema-biblioteca/src/main/Main.java
@@ -3,21 +3,43 @@ package main;
 import controller.BibliotecaController;
 import javax.swing.SwingUtilities;
 import view.CadastroLivro;
+import view.CadastroUsuario;
+import view.Emprestimo;
+import view.Devolução;
+import view.TelaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        // O Swing exige que as telas sejam iniciadas dentro desta Thread de evento
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // 1. Instancia a tela que criamos
-                CadastroLivro telaCadastroLivro = new CadastroLivro();
-                
-                // 2. Instancia o controller passando a view
-                new BibliotecaController(telaCadastroLivro);
+                // 1. instancia todas as views uma única vez
+                CadastroLivro   telaCadastroLivro   = new CadastroLivro();
+                CadastroUsuario telaCadastroUsuario  = new CadastroUsuario();
+                Emprestimo      telaEmprestimo       = new Emprestimo();
+                Devolução       telaDevolucao        = new Devolução();
 
-                //exibe a tela
-                telaCadastroLivro.setVisible(true);
+                // 2. instancia o controller passando todas as views (listeners gerados em cada tela)
+                new BibliotecaController(
+                    telaCadastroLivro,
+                    telaCadastroUsuario,
+                    telaEmprestimo,
+                    telaDevolucao
+                );
+
+                // 3. cria a TelaPrincipal com as mesmas instâncias já conectadas
+                TelaPrincipal telaPrincipal = new TelaPrincipal(
+                    telaCadastroUsuario,
+                    telaCadastroLivro,
+                    telaEmprestimo,
+                    telaDevolucao
+                );
+
+                // 4. salva referência global para o botão sair das outras telas voltarem aqui
+                TelaPrincipal.instancia = telaPrincipal;
+
+                // 5. exibe apenas a tela principal para começar
+                telaPrincipal.setVisible(true);
             }
         });
     }
